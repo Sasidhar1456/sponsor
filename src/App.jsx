8909f146d8219e85ad1617c87f5339ed9131c12b
Sasidhar Pinjala <98568230+Sasidhar1456@users.noreply.github.com>
2023-07-09 12:26:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import pic from './Sponsor.png'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+          <img src={pic}  className="logo" alt="Vite logo" />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
